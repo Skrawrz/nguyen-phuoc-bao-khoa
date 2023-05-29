91857a160cceb702abc1021b074e6fec61620f75
Skrawrz <npbkhoa@gmail.com>
2023-05-29 16:24:41 +0700

    add

---
+++ HTML-Starter/src/assets/styles/config/unility/minHeight.js
@@ -0,0 +1,16 @@
+const minHeight = {
+  0: '0',
+  full: '100%',
+  screen: '100vh',
+  banner: '456px',
+  500:'500px',
+  400:'400px',
+  450:'450px',
+  480:'480px',
+  470:'470px',
+  350:'350px',
+  150: '150px'
+}
+module.exports = {
+  minHeight
+}
---
+++ HTML-Starter/src/assets/styles/config/unility/minWidth.js
@@ -0,0 +1,12 @@
+const minWidth = {
+  0: '0',
+  full: '100%',
+  min: 'min-content',
+  max: 'max-content',
+  200: '200px',
+  150: '150px',
+  950: '950px'
+}
+module.exports = {
+  minWidth
+}
